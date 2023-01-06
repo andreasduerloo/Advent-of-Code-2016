c32e5e1a3df6af1dc4567e06bf141e2dd61338f7
Andreas Duerloo <andreasduerloo@gmail.com>
2023-01-06 18:56:49 +0100

    More cleanup

--- day01-hq/src/main.rs
+++ day01-hq/src/main.rs
@@ -4,13 +4,10 @@ use day01_hq::*;
 
 fn main() {
     if let Ok(content) = fs::read_to_string("input.txt") {
-        let instructions: Vec<&str> = content.split(", ").collect();
         let mut state: (Position, Direction, bool) = ((0, 0), Direction::North, false);
-        let mut history: History = HashSet::new();
-        
-        history.insert(state.0);
+        let mut history: History = HashSet::from([state.0]);
 
-        instructions.iter().for_each(|s| new_position(&mut state, s.trim(), &mut history));
+        content.split(", ").for_each(|s| new_position(&mut state, s.trim(), &mut history));
 
         println!("Distance from origin: {}", distance_to_origin(&state.0));
     }
