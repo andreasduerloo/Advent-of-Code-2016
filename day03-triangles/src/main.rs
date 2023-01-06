9046c590d9c8c860953ee90c1bca5e67a0e4d4ab
Andreas Duerloo <andreasduerloo@gmail.com>
2023-01-06 21:30:39 +0100

    Day 3, second star

--- day03-triangles/src/main.rs
+++ day03-triangles/src/main.rs
@@ -2,15 +2,32 @@ use std::fs;
 
 fn main() {
     if let Ok(content) = fs::read_to_string("input.txt") {
-        // let mut possible: usize = 0;
+        let content_copy = content.clone();
 
-        let possible = content.lines()
+        let possible1: usize = content.lines()
             .map(|s| string_to_vec(s))
             .map(|v| possible(v))
-            .reduce(|acc, val| acc + val)
-            .unwrap();
+            .sum();
 
-        println!("First star: {}", possible);
+        println!("First star: {}", possible1);
+
+        // Second star
+
+        let mut content_lines: Vec<&str> = content_copy.lines().collect();
+        let mut possible2: usize = 0;
+
+        while content_lines.len() != 0 {
+            let block: Vec<Vec<usize>> = content_lines.drain(0..3)
+                .map(|s| string_to_vec(s))
+                .collect();
+
+            for i in 0..3 {
+                let temp_vec = vec![block[0][i], block[1][i], block[2][i]];
+                possible2 += possible(temp_vec);
+            }
+        }
+
+        println!("Second star: {}", possible2);
     }
 }
 
@@ -29,3 +46,15 @@ fn possible(mut sides: Vec<usize>) -> usize {
         0
     }
 }
+
+// https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
+// fn transpose(v: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+//     let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+
+//     (0..3).map(|_| {
+//         iters.iter_mut()
+//             .map(|n| n.next().unwrap())
+//             .collect::<Vec<usize>>()
+//     })
+//     .collect()
+// }
